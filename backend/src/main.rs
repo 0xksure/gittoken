d7436003ca1bf16ba03571d35996008bcbcac6c6
kristofferberg-vipps <kristoffer.hovland.berg@vipps.no>
2021-04-13 22:13:15 +0200

    Parse access token and begin work on diesel to store github information

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,42 +4,79 @@
 extern crate rocket;
 use log::info;
 use reqwest;
+use reqwest::header::ACCEPT;
 use rocket::http::Method;
 use rocket::response::Redirect;
 use rocket::Request;
+use rocket::State;
+use serde::{Deserialize, Serialize};
 
-struct API {
-    client_id: String,
-    client_secret: String,
+use envconfig::Envconfig;
+use std::env;
+
+#[derive(Envconfig)]
+struct Config {
+    #[envconfig(from = "GITHUB_OAUTH_CLIENT_ID")]
+    pub client_id: String,
+    #[envconfig(from = "GITHUB_OAUTH_SECRET")]
+    pub client_secret: String,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
 struct Oauth {
     client_id: String,
     client_secret: String,
     code: String,
 }
 
+#[derive(Debug, Deserialize)]
+struct AccessTokenResponse {
+    access_token: String,
+    refresh_token: String,
+}
+
 #[get("/github/callback?<code>")]
-fn github_callback(code: Option<String>) -> Redirect {
+fn github_callback(cfg: State<Config>, code: Option<String>) -> Redirect {
     let _gcode = match code {
         Some(res) => res,
         None => return Redirect::to(format!("http://localhost:5000/login/error")),
     };
     info!("ok let's check who we are dealing with");
 
-    let client = reqwest::Client::new();
-    let res = client
-        .post("http://httpbin.org/post")
-        .body("the exact body that is sent")
+    let github_post = Oauth {
+        client_id: cfg.client_id.to_string(),
+        client_secret: cfg.client_secret.to_string(),
+        code: _gcode,
+    };
+    let res = match reqwest::blocking::Client::new()
+        .post("https://github.com/login/oauth/access_token")
+        .header(ACCEPT, "application/json")
+        .json(&github_post)
         .send()
-        .await?;
+    {
+        Ok(res) => res,
+        Err(err) => {
+            info!("error when retrieving access token: {}", err.to_string());
+            return Redirect::to(format!("http://localhost:5000/login/error"));
+        }
+    };
+
+    let resp_code = res.status();
+    if resp_code != reqwest::StatusCode::OK {
+        info!("access token response code: {}", resp_code.to_string());
+        return Redirect::to(format!("http://localhost:5000/login/error"));
+    }
+    let access_token_resp = res.json::<AccessTokenResponse>();
+    info!("bytes: {:?}", access_token_resp);
 
     Redirect::to(format!("http://localhost:5000/login/success"))
 }
 
 fn main() {
     log::info!("[root] info");
+    let cfg = Config::init_from_env().unwrap();
     rocket::ignite()
+        .manage(cfg)
         .mount("/", routes![github_callback])
         .launch();
 }
